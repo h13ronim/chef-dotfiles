31ab9ccb4620287cbe03a762a324222dfa3a1bec
Marcin Michalowski <marcin@stick.pl>
2016-07-28 15:41:52 +0200

    Add Karabiner recipe

---
+++ recipes/karabiner.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: dotfiles
+# Recipe:: karabiner
+#
+# Copyright 2016, Marcin Michałowski
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+homebrew_cask 'karabiner'
+
+# https://pqrs.org/osx/karabiner/document.html.en#commandlineinterface
+execute "/Applications/Karabiner.app/Contents/Library/bin/karabiner enable remap.commandTab2f5" do
+  user node['user']['id']
+end
